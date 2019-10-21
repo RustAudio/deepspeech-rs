e784a8f90682c4b67faa982b5e5d2e44ed57aee8
Atul Bhosale <atul1bhosale@gmail.com>
2019-10-21 23:17:32 +0530

    Format code using 'cargo fmt'

--- examples/client.rs
+++ examples/client.rs
@@ -1,26 +1,26 @@
-extern crate deepspeech;
 extern crate audrey;
+extern crate deepspeech;
 
-use std::path::Path;
 use std::env::args;
 use std::fs::File;
+use std::path::Path;
 use std::time::Instant;
 
-use deepspeech::Model;
 use audrey::read::Reader;
 use audrey::sample::interpolate::{Converter, Linear};
 use audrey::sample::signal::{from_iter, Signal};
+use deepspeech::Model;
 
 // These constants are taken from the C++ sources of the client.
 
-const BEAM_WIDTH :u16 = 500;
+const BEAM_WIDTH: u16 = 500;
 
-const LM_WEIGHT :f32 = 0.75;
-const VALID_WORD_COUNT_WEIGHT :f32 = 1.85;
+const LM_WEIGHT: f32 = 0.75;
+const VALID_WORD_COUNT_WEIGHT: f32 = 1.85;
 
 // The model has been trained on this specific
 // sample rate.
-const SAMPLE_RATE :u32 = 16_000;
+const SAMPLE_RATE: u32 = 16_000;
 
 /*
 TODO list:
@@ -30,64 +30,76 @@ TODO list:
 * use clap or something to parse the command line arguments
 */
 fn main() {
-	let start = Instant::now();
-	let model_dir_str = args().nth(1)
-		.expect("Please specify model dir");
-	let audio_file_path = args().nth(2)
-		.expect("Please specify an audio file to run STT on");
-	let dir_path = Path::new(&model_dir_str);
-	let mut m = Model::load_from_files(
-		&dir_path.join("output_graph.pb"),
-		&dir_path.join("alphabet.txt"),
-		BEAM_WIDTH).unwrap();
-	m.enable_decoder_with_lm(
-		&dir_path.join("lm.binary"),
-		&dir_path.join("trie"),
-		LM_WEIGHT,
-		VALID_WORD_COUNT_WEIGHT);
+    let start = Instant::now();
+    let model_dir_str = args().nth(1).expect("Please specify model dir");
+    let audio_file_path = args()
+        .nth(2)
+        .expect("Please specify an audio file to run STT on");
+    let dir_path = Path::new(&model_dir_str);
+    let mut m = Model::load_from_files(
+        &dir_path.join("output_graph.pb"),
+        &dir_path.join("alphabet.txt"),
+        BEAM_WIDTH,
+    )
+    .unwrap();
+    m.enable_decoder_with_lm(
+        &dir_path.join("lm.binary"),
+        &dir_path.join("trie"),
+        LM_WEIGHT,
+        VALID_WORD_COUNT_WEIGHT,
+    );
 
-	let initialized_time = Instant::now();
-	println!("Model initialized in {:?}.", initialized_time - start);
+    let initialized_time = Instant::now();
+    println!("Model initialized in {:?}.", initialized_time - start);
 
-	let audio_file = File::open(audio_file_path).unwrap();
-	let mut reader = Reader::new(audio_file).unwrap();
-	let desc = reader.description();
-	assert_eq!(1, desc.channel_count(),
-		"The channel count is required to be one, at least for now");
+    let audio_file = File::open(audio_file_path).unwrap();
+    let mut reader = Reader::new(audio_file).unwrap();
+    let desc = reader.description();
+    assert_eq!(
+        1,
+        desc.channel_count(),
+        "The channel count is required to be one, at least for now"
+    );
 
-	// Obtain the buffer of samples
-	let audio_buf :Vec<_> = if desc.sample_rate() == SAMPLE_RATE {
-		reader.samples().map(|s| s.unwrap()).collect()
-	} else {
-		// We need to interpolate to the target sample rate
-		let interpolator = Linear::new([0i16], [0]);
-		let conv = Converter::from_hz_to_hz(
-			from_iter(reader.samples::<i16>().map(|s| [s.unwrap()])),
-			interpolator,
-			desc.sample_rate() as f64,
-			SAMPLE_RATE as f64);
-		conv.until_exhausted().map(|v| v[0]).collect()
-	};
+    // Obtain the buffer of samples
+    let audio_buf: Vec<_> = if desc.sample_rate() == SAMPLE_RATE {
+        reader.samples().map(|s| s.unwrap()).collect()
+    } else {
+        // We need to interpolate to the target sample rate
+        let interpolator = Linear::new([0i16], [0]);
+        let conv = Converter::from_hz_to_hz(
+            from_iter(reader.samples::<i16>().map(|s| [s.unwrap()])),
+            interpolator,
+            desc.sample_rate() as f64,
+            SAMPLE_RATE as f64,
+        );
+        conv.until_exhausted().map(|v| v[0]).collect()
+    };
 
-	let len_seconds = audio_buf.len() as f64 / SAMPLE_RATE as f64;
+    let len_seconds = audio_buf.len() as f64 / SAMPLE_RATE as f64;
 
-	let decoded_time = Instant::now();
+    let decoded_time = Instant::now();
 
-	println!("Decoding done in {:?}. Sample length {}s. Running STT.",
-		decoded_time - initialized_time, len_seconds);
+    println!(
+        "Decoding done in {:?}. Sample length {}s. Running STT.",
+        decoded_time - initialized_time,
+        len_seconds
+    );
 
-	// Run the speech to text algorithm
-	let result = m.speech_to_text(&audio_buf).unwrap();
+    // Run the speech to text algorithm
+    let result = m.speech_to_text(&audio_buf).unwrap();
 
-	let text_time = Instant::now();
+    let text_time = Instant::now();
 
-	let elapsed = text_time - decoded_time;
+    let elapsed = text_time - decoded_time;
 
-	let elapsed_f = elapsed.subsec_micros() as f64 / 1_000_000.0
-		+ elapsed.as_secs() as f64;
-	println!("STT done in {:?}. Real time factor {:.5}", elapsed, elapsed_f / len_seconds);
+    let elapsed_f = elapsed.subsec_micros() as f64 / 1_000_000.0 + elapsed.as_secs() as f64;
+    println!(
+        "STT done in {:?}. Real time factor {:.5}",
+        elapsed,
+        elapsed_f / len_seconds
+    );
 
-	// Output the result
-	println!("{}", result);
-
+    // Output the result
+    println!("{}", result);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,258 +2,244 @@
 Bindings to the DeepSpeech library
 */
 
-extern crate libc;
 extern crate deepspeech_sys;
+extern crate libc;
 
+use deepspeech_sys as ds;
 use std::ffi::CStr;
 use std::fmt;
-use std::path::Path;
-use std::ops::Drop;
-use std::ptr;
 use std::mem::forget;
+use std::ops::Drop;
+use std::path::Path;
+use std::ptr;
 use std::slice;
-use deepspeech_sys as ds;
 
 pub struct Model {
-	model :* mut ds::ModelState,
+    model: *mut ds::ModelState,
 }
 
 pub struct Metadata {
-	metadata :* mut ds::Metadata,
+    metadata: *mut ds::Metadata,
 }
 
 #[repr(transparent)]
 pub struct MetadataItem {
-	metadata_item :ds::MetadataItem,
+    metadata_item: ds::MetadataItem,
 }
 
-fn path_to_buf(p :&Path) -> Vec<u8> {
-	let s = p.to_str().unwrap();
-	let mut v = Vec::with_capacity(s.len());
-	v.extend_from_slice(s.as_bytes());
-	v.push(0);
-	v
+fn path_to_buf(p: &Path) -> Vec<u8> {
+    let s = p.to_str().unwrap();
+    let mut v = Vec::with_capacity(s.len());
+    v.extend_from_slice(s.as_bytes());
+    v.push(0);
+    v
 }
 
 impl Model {
-	/// Load a DeepSpeech model from the specified model and alphabet file paths
-	pub fn load_from_files(model_path :&Path, alphabet_path :&Path, beam_width :u16) -> Result<Self, ()> {
-		let mp = path_to_buf(model_path);
-		let ap = path_to_buf(alphabet_path);
-		let mut model = ptr::null_mut();
-		let ret = unsafe {
-			ds::DS_CreateModel(
-				mp.as_ptr() as _,
-				ap.as_ptr() as _,
-				beam_width as _,
-				&mut model)
-		};
-		if ret != 0 {
-			return Err(());
-		}
-		Ok(Model {
-			model,
-		})
-	}
+    /// Load a DeepSpeech model from the specified model and alphabet file paths
+    pub fn load_from_files(
+        model_path: &Path,
+        alphabet_path: &Path,
+        beam_width: u16,
+    ) -> Result<Self, ()> {
+        let mp = path_to_buf(model_path);
+        let ap = path_to_buf(alphabet_path);
+        let mut model = ptr::null_mut();
+        let ret = unsafe {
+            ds::DS_CreateModel(
+                mp.as_ptr() as _,
+                ap.as_ptr() as _,
+                beam_width as _,
+                &mut model,
+            )
+        };
+        if ret != 0 {
+            return Err(());
+        }
+        Ok(Model { model })
+    }
 
-	/// Load a KenLM language model from a file and enable decoding using beam scoring
-	pub fn enable_decoder_with_lm(&mut self, language_model_path :&Path,
-			trie_path :&Path, weight :f32, valid_word_count_weight :f32) {
-		let lp = path_to_buf(language_model_path);
-		let tp = path_to_buf(trie_path);
-		unsafe {
-			ds::DS_EnableDecoderWithLM(
-				self.model,
-				lp.as_ptr() as _,
-				tp.as_ptr() as _,
-				weight,
-				valid_word_count_weight);
-		}
-	}
+    /// Load a KenLM language model from a file and enable decoding using beam scoring
+    pub fn enable_decoder_with_lm(
+        &mut self,
+        language_model_path: &Path,
+        trie_path: &Path,
+        weight: f32,
+        valid_word_count_weight: f32,
+    ) {
+        let lp = path_to_buf(language_model_path);
+        let tp = path_to_buf(trie_path);
+        unsafe {
+            ds::DS_EnableDecoderWithLM(
+                self.model,
+                lp.as_ptr() as _,
+                tp.as_ptr() as _,
+                weight,
+                valid_word_count_weight,
+            );
+        }
+    }
 
-	/// Perform speech-to-text using the model
-	///
-	/// The input buffer must consist of mono 16-bit samples.
-	/// The sample rate is not freely chooseable but a property
-	/// of the model files.
-	pub fn speech_to_text(&mut self, buffer :&[i16]) -> Result<String, std::string::FromUtf8Error> {
-		let r = unsafe {
-			let ptr = ds::DS_SpeechToText(
-				self.model,
-				buffer.as_ptr(),
-				buffer.len() as _);
-			let s = CStr::from_ptr(ptr);
-			let mut v = Vec::new();
-			v.extend_from_slice(s.to_bytes());
-			ds::DS_FreeString(ptr);
-			v
-		};
-		String::from_utf8(r)
-	}
+    /// Perform speech-to-text using the model
+    ///
+    /// The input buffer must consist of mono 16-bit samples.
+    /// The sample rate is not freely chooseable but a property
+    /// of the model files.
+    pub fn speech_to_text(&mut self, buffer: &[i16]) -> Result<String, std::string::FromUtf8Error> {
+        let r = unsafe {
+            let ptr = ds::DS_SpeechToText(self.model, buffer.as_ptr(), buffer.len() as _);
+            let s = CStr::from_ptr(ptr);
+            let mut v = Vec::new();
+            v.extend_from_slice(s.to_bytes());
+            ds::DS_FreeString(ptr);
+            v
+        };
+        String::from_utf8(r)
+    }
 
-	/// Perform speech-to-text using the model, getting extended metadata
-	///
-	/// The input buffer must consist of mono 16-bit samples.
-	/// The sample rate is not freely chooseable but a property
-	/// of the model files.
-	pub fn speech_to_text_with_metadata(&mut self, buffer :&[i16]) -> Result<Metadata, ()> {
-		let ptr = unsafe {
-			ds::DS_SpeechToTextWithMetadata(
-				self.model,
-				buffer.as_ptr(),
-				buffer.len() as _)
-		};
-		Ok(Metadata {
-			metadata : ptr
-		})
-	}
+    /// Perform speech-to-text using the model, getting extended metadata
+    ///
+    /// The input buffer must consist of mono 16-bit samples.
+    /// The sample rate is not freely chooseable but a property
+    /// of the model files.
+    pub fn speech_to_text_with_metadata(&mut self, buffer: &[i16]) -> Result<Metadata, ()> {
+        let ptr = unsafe {
+            ds::DS_SpeechToTextWithMetadata(self.model, buffer.as_ptr(), buffer.len() as _)
+        };
+        Ok(Metadata { metadata: ptr })
+    }
 
-	/// Set up a state for streaming inference
-	pub fn create_stream(&mut self) -> Result<Stream, ()> {
-		let mut ptr = ptr::null_mut();
-		let ret = unsafe {
-			ds::DS_CreateStream(
-				self.model,
-				&mut ptr,
-			)
-		};
-		if ret != 0 {
-			return Err(());
-		}
-		Ok(Stream {
-			stream : ptr
-		})
-	}
+    /// Set up a state for streaming inference
+    pub fn create_stream(&mut self) -> Result<Stream, ()> {
+        let mut ptr = ptr::null_mut();
+        let ret = unsafe { ds::DS_CreateStream(self.model, &mut ptr) };
+        if ret != 0 {
+            return Err(());
+        }
+        Ok(Stream { stream: ptr })
+    }
 }
 
 impl Drop for Model {
-	fn drop(&mut self) {
-		unsafe {
-			ds::DS_FreeModel(self.model);
-		}
-	}
+    fn drop(&mut self) {
+        unsafe {
+            ds::DS_FreeModel(self.model);
+        }
+    }
 }
 
 impl Drop for Metadata {
-	fn drop(&mut self) {
-		unsafe {
-			ds::DS_FreeMetadata(self.metadata);
-		}
-	}
+    fn drop(&mut self) {
+        unsafe {
+            ds::DS_FreeMetadata(self.metadata);
+        }
+    }
 }
 
 impl Metadata {
-	pub fn items(&self) -> &[MetadataItem] {
-		unsafe {
-			let ptr = (*self.metadata).items as * const MetadataItem;
-			slice::from_raw_parts(ptr, self.num_items() as usize)
-		}
-	}
+    pub fn items(&self) -> &[MetadataItem] {
+        unsafe {
+            let ptr = (*self.metadata).items as *const MetadataItem;
+            slice::from_raw_parts(ptr, self.num_items() as usize)
+        }
+    }
 
-	pub fn num_items(&self) -> i32 {
-		unsafe {
-			(*self.metadata).num_items
-		}
-	}
+    pub fn num_items(&self) -> i32 {
+        unsafe { (*self.metadata).num_items }
+    }
 
-	pub fn confidence(&self) -> f64 {
-		unsafe {
-			(*self.metadata).confidence
-		}
-	}
+    pub fn confidence(&self) -> f64 {
+        unsafe { (*self.metadata).confidence }
+    }
 }
 
 impl MetadataItem {
-	pub fn character(&self) -> Result<&str, std::str::Utf8Error> {
-		unsafe {
-			let slice = CStr::from_ptr(self.metadata_item.character);
-			slice.to_str()
-		}
-	}
+    pub fn character(&self) -> Result<&str, std::str::Utf8Error> {
+        unsafe {
+            let slice = CStr::from_ptr(self.metadata_item.character);
+            slice.to_str()
+        }
+    }
 
-	pub fn timestep(&self) -> i32 {
-		self.metadata_item.timestep
-	}
+    pub fn timestep(&self) -> i32 {
+        self.metadata_item.timestep
+    }
 
-	pub fn start_time(&self) -> f32 {
-		self.metadata_item.start_time
-	}
+    pub fn start_time(&self) -> f32 {
+        self.metadata_item.start_time
+    }
 }
 
 impl fmt::Display for Metadata {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let mut s = String::new();
-		for item in self.items() {
-			s += item.character().unwrap();
-		}
-		write!(f, "{}", s)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for item in self.items() {
+            s += item.character().unwrap();
+        }
+        write!(f, "{}", s)
+    }
 }
 
 pub struct Stream {
-	stream :* mut ds::StreamingState,
+    stream: *mut ds::StreamingState,
 }
 
 impl Stream {
-	/// Feed audio samples to the stream
-	///
-	/// The input buffer must consist of mono 16-bit samples.
-	pub fn feed_audio(&mut self, buffer :&[i16]) {
-		unsafe {
-			ds::DS_FeedAudioContent(self.stream, buffer.as_ptr(), buffer.len() as _);
-		}
-	}
+    /// Feed audio samples to the stream
+    ///
+    /// The input buffer must consist of mono 16-bit samples.
+    pub fn feed_audio(&mut self, buffer: &[i16]) {
+        unsafe {
+            ds::DS_FeedAudioContent(self.stream, buffer.as_ptr(), buffer.len() as _);
+        }
+    }
 
-	/// Decodes the intermediate state of what has been spoken up until now
-	///
-	/// Note that as of DeepSpeech version 0.2.0,
-	/// this function is non-trivial as the decoder can't do streaming yet.
-	pub fn intermediate_decode(&mut self) -> Result<String, std::string::FromUtf8Error> {
-		let r = unsafe {
-			let ptr = ds::DS_IntermediateDecode(self.stream);
-			let s = CStr::from_ptr(ptr);
-			let mut v = Vec::new();
-			v.extend_from_slice(s.to_bytes());
-			ds::DS_FreeString(ptr);
-			v
-		};
-		String::from_utf8(r)
-	}
+    /// Decodes the intermediate state of what has been spoken up until now
+    ///
+    /// Note that as of DeepSpeech version 0.2.0,
+    /// this function is non-trivial as the decoder can't do streaming yet.
+    pub fn intermediate_decode(&mut self) -> Result<String, std::string::FromUtf8Error> {
+        let r = unsafe {
+            let ptr = ds::DS_IntermediateDecode(self.stream);
+            let s = CStr::from_ptr(ptr);
+            let mut v = Vec::new();
+            v.extend_from_slice(s.to_bytes());
+            ds::DS_FreeString(ptr);
+            v
+        };
+        String::from_utf8(r)
+    }
 
-	/// Deallocates the stream and returns the decoded text
-	pub fn finish(self) -> Result<String, std::string::FromUtf8Error> {
-		let r = unsafe {
-			let ptr = ds::DS_FinishStream(self.stream);
-			let s = CStr::from_ptr(ptr);
-			let mut v = Vec::new();
-			v.extend_from_slice(s.to_bytes());
-			ds::DS_FreeString(ptr);
-			v
-		};
-		// Don't run the destructor for self,
-		// as DS_FinishStream already does it for us
-		forget(self);
-		String::from_utf8(r)
-	}
+    /// Deallocates the stream and returns the decoded text
+    pub fn finish(self) -> Result<String, std::string::FromUtf8Error> {
+        let r = unsafe {
+            let ptr = ds::DS_FinishStream(self.stream);
+            let s = CStr::from_ptr(ptr);
+            let mut v = Vec::new();
+            v.extend_from_slice(s.to_bytes());
+            ds::DS_FreeString(ptr);
+            v
+        };
+        // Don't run the destructor for self,
+        // as DS_FinishStream already does it for us
+        forget(self);
+        String::from_utf8(r)
+    }
 
-	/// Deallocates the stream and returns the extended metadata
-	pub fn finish_with_metadata(self) -> Result<Metadata, ()> {
-		let ptr = unsafe {
-			ds::DS_FinishStreamWithMetadata(self.stream)
-		};
-		// Don't run the destructor for self,
-		// as DS_FinishStream already does it for us
-		forget(self);
-		Ok(Metadata {
-			metadata : ptr
-		})
-	}
+    /// Deallocates the stream and returns the extended metadata
+    pub fn finish_with_metadata(self) -> Result<Metadata, ()> {
+        let ptr = unsafe { ds::DS_FinishStreamWithMetadata(self.stream) };
+        // Don't run the destructor for self,
+        // as DS_FinishStream already does it for us
+        forget(self);
+        Ok(Metadata { metadata: ptr })
+    }
 }
 
 impl Drop for Stream {
-	fn drop(&mut self) {
-		unsafe {
-			ds::DS_FreeStream(self.stream);
-		}
-	}
+    fn drop(&mut self) {
+        unsafe {
+            ds::DS_FreeStream(self.stream);
+        }
+    }
 }
