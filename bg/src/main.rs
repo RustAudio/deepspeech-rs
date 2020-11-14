26af9bca27f8443eefab8ec9412b3972b8632dbc
James Elford <james.p.elford@gmail.com>
2020-11-14 12:59:49 +0000

    updated deepspeech to latest (0.9.1) and re-ran bg tool
    nb. this reverts the rename of deepspeech.h -> mozilla_voice_stt.h [0]
    
    [0] ae0cf8db6aeeaa93ab2bbf89e9e76ba52347ae5e, merged in https://github.com/mozilla/DeepSpeech/pull/3280

--- bg/src/main.rs
+++ bg/src/main.rs
@@ -190,7 +190,7 @@ fn add_spacings(tokens: TokenStream) -> String {
 fn main() {
 	let bindings = bindgen::Builder::default()
 		.header("stddef.h")
-		.header("sys/deepspeech/native_client/mozilla_voice_stt.h")
+		.header("sys/deepspeech/native_client/deepspeech.h")
 		.clang_args(&["-x", "c++", "-std=c++11"])
 		.generate()
 		.expect("Couldn't generate bindings");
